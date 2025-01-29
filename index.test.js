0f7832ee60fbf2ca711c137f1d1b42af4886c2dd
Alberto <albertogilsanjuan@gmail.com>
2025-01-29 02:11:49 +0100

    FIX: Room Tests

--- index.test.js
+++ index.test.js
@@ -7,29 +7,29 @@ const bookings = [
         checkIn: new Date('2025-01-27'),
         checkOut: new Date('2025-01-30'),
         discount: 20,
-        room: room
+        room: new Room('Room 1', [], 10000, 10)
     }, {
         name: 'Paco',
         email: 'paco@gmail.com',
         checkIn: new Date('2025-02-10'),
         checkOut: new Date('2025-02-15'),
         discount: 15,
-        room: room
+        room: new Room('Room 2', [], 10000, 10)
     }, {
         name: 'Luis',
         email: 'luis@gmail.com',
         checkIn: new Date('2025-03-18'),
         checkOut: new Date('2025-03-22'),
         discount: 10,
-        room: room
+        room: new Room('Room 3', [], 10000, 10)
     }
 ]
 
 const room = new Room('Room 1', bookings, 10000, 10)
 const roomWithoutBookings = new Room('Room 2', [], 15000, 20)
 
-const rooms = [room, room]
-const roomsWithoutBookings = [roomWithoutBookings, roomWithoutBookings]
+const rooms = [room]
+const roomsWithoutBookings = [roomWithoutBookings]
 
 describe('TEST DE ROOMS', () => {
 
@@ -39,21 +39,21 @@ describe('TEST DE ROOMS', () => {
     })
 
     test('OccupancyPercentage Method', () => {
-        expect(room.occupancyPercentage(new Date('2025-01-10'), new Date('2025-01-15'))).toBe(0)
+        expect(roomWithoutBookings.occupancyPercentage(new Date('2025-01-10'), new Date('2025-01-15'))).toBe(0)
         expect(room.occupancyPercentage(new Date('2025-02-11'), new Date('2025-02-14'))).toBe(100)
         expect(room.occupancyPercentage(new Date('2025-03-16'), new Date('2025-03-19'))).toBe(50)
     })
 
     test('TotalOccupancyPercentage Method', () => {
         expect(Room.totalOccupancyPercentage(roomsWithoutBookings, new Date('2025-01-27'), new Date('2025-01-31'))).toBe(0)
-        expect(Room.totalOccupancyPercentage(rooms, new Date('2025-02-11'), new Date('2025-02-14'))).toBe(100)
+        expect(Room.totalOccupancyPercentage(rooms, new Date('2025-02-12'), new Date('2025-02-14'))).toBe(100)
         expect(Room.totalOccupancyPercentage(rooms, new Date('2025-03-16'), new Date('2025-03-19'))).toBe(50)
     })
 
     test('AvailableRooms Method', () => {
-        expect(Room.availableRooms(roomsWithoutBookings, new Date('2025-01-10'), new Date('2025-01-15'))).toBe(roomsWithoutBookings)
-        expect(Room.availableRooms(rooms, new Date('2025-02-20'), new Date('2025-03-10'))).toBe(rooms)
-        expect(Room.availableRooms(rooms, new Date('2025-01-11'), new Date('2025-01-14'))).toBe([])
+        expect(Room.availableRooms(roomsWithoutBookings, new Date('2025-01-10'), new Date('2025-01-15'))).toStrictEqual(roomsWithoutBookings)
+        expect(Room.availableRooms(rooms, new Date('2025-02-20'), new Date('2025-03-10'))).toStrictEqual(rooms)
+        expect(Room.availableRooms(rooms, new Date('2025-01-11'), new Date('2025-01-14'))).toStrictEqual([])
     })
 })
 
@@ -62,8 +62,10 @@ const booking = new Booking('Alberto', 'alberto@gmail.com', new Date('2025-01-01
 describe('TEST DE BOOKINGS', () => {
 
     test('GetFee Method', () => {
-        const bookingDays = (booking.checkOut - booking.checkIn) / (1000 * 60 * 60 * 24)
-        const expectedFee = room.rate * (100 - (room.discount / 100) * (100 - (booking.discount / 100)) * bookingDays)
-        expect(booking.fee).toBe(expectedFee)
+        const bookingDays = (booking.checkOut - booking.checkIn) / (1000 * 60 * 60 * 24);
+        const roomRate = room.rate - (room.rate * room.discount / 100);
+        const discountedRate = roomRate - (roomRate * booking.discount / 100);
+        const expectedFee = Math.round(discountedRate * bookingDays);
+        expect(booking.fee).toBe(expectedFee);
     })
 })
